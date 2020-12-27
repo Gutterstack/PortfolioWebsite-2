c6cdbb5686ed998a29b244ba596e9dde2c18ac95
Gutterstack <uebelhorr@gmail.com>
2020-12-26 21:37:10 -0600

    added JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+// Mobile Viewport
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-
-
+// EventListener for resize event (user turns screen (lascape to portrait) or navigation move out of view on scroll)
+window.addEventListener('resize', () => {
+    //same code from above because we want the same result
+ let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
